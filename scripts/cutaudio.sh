bcbee20a6361717067387603c8a70c4d11969918
Hygor X. Araújo <hygor.x.a@gmail.com>
2014-01-14 11:57:52 -0200

    Atualizando script

--- scripts/cutaudio.sh
+++ scripts/cutaudio.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#extrai audio do video (mp4) para wav
+ffmpeg -i $1.mp4 -vn -ac 1 -ab 160k -ar 22050 $1.wav
+
 #pega tamanho total do audio em segundos
-FILE_LENGTH=$(sox $1.mp3 -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p')
+FILE_LENGTH=$(soxi -D $1.wav)
 var=$(echo $FILE_LENGTH|awk '{print int($1+0.5)}')
 
 
@@ -11,6 +14,31 @@ array=( 0 $(grep -o '<time>[0-9].*</time>' $1.index.xml | sed 's/\(<time>\|<\/ti
 #corta o audio
 for (( i = 0; i < ${#array[@]}; i++ ))
 do
+	if [[ i -eq 0 && ${array[1]} -eq 0 ]];
+	then
+		i=$[i+1]
+	fi
 	e=`expr ${array[i+1]} - ${array[i]}`
-	ffmpeg -ss ${array[i]} -t $e -i $1.mp3 -acodec copy $1_$i.mp3
+	ffmpeg -ss ${array[i]} -t $e -i $1.wav -acodec copy $1_$i.wav
+done
+
+#remove silêncio dos arquivos cortados
+for f in $1_[0-9]*.wav
+do
+	sox $f sil$f silence 1 0.1 1% -1 0.5 1%
+done
+
+mkdir transc
+for j in sil*.wav
+do
+	if [[ $(soxi -D $j) > 60 ]]
+	then
+		python ../spk_drztn.py $j
+		#move todos os audios segmentados de todos os clusters para a pasta transc
+		find ./${j%.*}/S* -type f -name '*.wav' -exec rename "s/S[0-9]*_/${j%.*}_/" {} \;
+		find ./${j%.*}/S* -iname '*.wav' -exec mv {} transc \;
+		rm -r ./${j%.*}/ #remove as pastas vazias dos clusters
+	else
+		mv $j transc
+	fi
 done
