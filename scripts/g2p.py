b7e75743d1813d15410a93aa236dbd901cf346a5
HygorX <hygor.x.a@gmail.com>
2014-06-02 10:14:44 -0300

    Atualizando script g2p e g2p_table

--- scripts/g2p.py
+++ scripts/g2p.py
@@ -7,10 +7,12 @@ Created on Apr 16, 2014
 
 import codecs
 import collections
+import os
 from itertools import product
 
 rulesFile = codecs.open('g2p_table.txt', 'r', encoding='utf8') #arquivo com representações fonéticas de cada letra
 wordsFile = codecs.open('words.txt', 'r', encoding='utf8') #arquivo com lista de palavras a serem utilizadas no dicionário (uma palavra por linha)
+tempDic = codecs.open('temp_dict.dic', 'w+', encoding='utf8') #arquivo com dicionário sem remoção de repetições
 resultDic = codecs.open('dictionary.dic', 'w', encoding='utf8') #arquivo onde será escrito o dicionário
 
 #inserção das representações fonéticas em uma estrutura de dicionário
@@ -18,16 +20,30 @@ dic = collections.defaultdict(list)
 for line in rulesFile:
     dic[line[0]].append(line[2:])
 
-resultDic.write("</s> [] sil\n")
-resultDic.write("<s> [] sil\n")
+dicFirstPos = tempDic.tell()
+tempDic.write("</s> [] sil\n")
+tempDic.write("<s> [] sil\n")
 #geração de todas as representações fonéticas de cada palavra contida em words.txt
 for word in wordsFile:
     word = word.strip()
     for x in product(*(dic[char] for char in word)):
         y = ' '.join(x).splitlines()
         y = ''.join(y)        
-        resultDic.write(word + '    ' + y + '\n' )
+        tempDic.write(word + '    ' + y + '  ' + '\n' )
+
+tempDic.seek(dicFirstPos)
+#remove linhas repetidas no dicionário
+seen = set()
+for line in tempDic:
+    line_lower = line.lower()
+    if line_lower not in seen:
+        seen.add(line_lower)
+resultDic.writelines(sorted(seen))
 
 rulesFile.close()
 wordsFile.close()
+tempDic.close()
 resultDic.close()
+os.remove('temp_dict.dic')
+
+print 'Dicionário gerado em dictionary.dic'
