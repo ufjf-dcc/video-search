482d136c6207f4ddfb2f6d120391d160d87f37af
HygorX <hygor.x.a@gmail.com>
2014-07-17 10:45:25 -0300

    substituindo cutaudio.sh por mp4totxt.sh e atualizando scripts

--- scripts/mp4totxt.sh
+++ scripts/mp4totxt.sh
@@ -7,16 +7,15 @@ ffmpeg -i $1.mp4 -vn -ac 1 -ab 160k -ar 16000 $1.wav
 FILE_LENGTH=$(soxi -D $1.wav)
 var=$(echo $FILE_LENGTH|awk '{print int($1+0.5)}')
 
-
 #preenche um array com os tempos de 0 até o tamanho do audio
-array=( 0 $(grep -o '<time>[0-9].*</time>' $1.index.xml | sed 's/\(<time>\|<\/time>\)//g') $var )
+array=( 0 $(grep -o '<time>[0-9].*</time>' $1.index | sed 's/\(<time>\|<\/time>\)//g') $var )
 
 #corta o audio
 for (( i = 0; i < ${#array[@]}; i++ ))
 do
 	if [[ i -eq 0 && ${array[1]} -eq 0 ]];
 	then
-		i=$[i+1]
+	i=$[i+1]
 	fi
 	e=`expr ${array[i+1]} - ${array[i]}`
 	ffmpeg -ss ${array[i]} -t $e -i $1.wav -acodec copy $1_$i.wav
@@ -34,30 +33,37 @@ do
 done
 
 #cria a pasta transc para armazenar os áudios que serão usados para transcrição
-mkdir transc 
+mkdir transc_$1
 for j in sil*.wav
 do
 	if [[ "$(echo $(soxi -D $j)|awk '{print int($1+0.5)}')" -gt "45" ]] #caso o áudio tenha mais que 45seg faz a diarização
 	then
 		#faz a diarização
-		python ../spk_drztn.py $j
-		#move todos os audios segmentados de todos os clusters para a pasta transc
+		python spk_drztn.py $j
+		#move todos os audios segmentados de todos os clusters para a pasta transc_$1
 		find ./${j%.*}/S* -type f -name '*.wav' -exec rename "s/S[0-9]*_/${j%.*}_/" {} \;
-		find ./${j%.*}/S* -iname '*.wav' -exec mv {} transc \;
+		find ./${j%.*}/S* -iname '*.wav' -exec mv {} transc_$1 \;
 		#remove as pastas vazias dos clusters
 		rm -r ./${j%.*}/
-	else #se não apenas move o áudio para transc
-		mv $j transc
+	else #se não apenas move o áudio para transc_$1
+		mv $j transc_$1
 	fi
 done
 
-cd transc
+rm sil*
+rm *.wav
+
+cd transc_$1
 #copia o caminho dos áudios a serem transcritos para o arquivo lista do julius
 for f in *.wav
 do
 	echo "$(readlink -f $f)"
-done > /usr/local/bin/pt_br/1.7/lista
+done > /usr/local/bin/pt_BR/1.7/lista
 #faz a transcrição
-julius -C /usr/local/bin/pt_BR/1.7/julius.jconf | grep pass1_best: > transcrito.txt
+julius -C /usr/local/bin/pt_BR/1.7/julius.jconf | grep pass1_best: > transcrito_$1.txt
 #remove 'pass1_best:' do arquivo transcrito
-sed -i 's/pass1_best:  //' transcrito.txt
+sed -i 's/pass1_best: //' transcrito_$1.txt
+
+mv transcrito_$1.txt ..
+cd ..
+rm -r transc_$1
--- scripts/rdfgen.py
+++ scripts/rdfgen.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on Apr 9, 2014
 
@@ -7,20 +8,23 @@ Created on Apr 9, 2014
 import xml.etree.ElementTree as ET
 import re
 import sys
+import os
+import codecs
 
-#para executar: python rdfgen.py <nome do arquivo xml sem extensao>
-tree1 = ET.parse(sys.argv[1] + '.xml')
-root = tree1.getroot()
+fileName, fileExtension = os.path.splitext(sys.argv[1])
+
+tree = ET.parse(fileName + fileExtension)
+root = tree.getroot()
 
 TITLE = root.find('general/title/string').text
 #ABSTRACT = root.find('').text
 DATE = root.find('lifecycle/contribute/date').text
+#REFERENCES = root.find('').text
+with codecs.open("transcrito_" + fileName + ".txt", "r", encoding='utf8') as transcrito:
+    DESCRIPTION = transcrito.read().replace('\n', ' ')
 COURSENAME = root.find('videoaula/educational/course/title/string').text
 COURSECODE = root.find('videoaula/educational/course/code').text
 KEYWORDS = root.find('general/keyword/string').text
-TAXONOMY = root.find('classification/taxonPath/taxon/entry/string').text
-#REFERENCES = root.find('').text
-DESCRIPTION = root.find('general/description/string').text
 c = 1
 for creator in root.findall('lifecycle/contribute'):
     d = creator.find('entity').text
@@ -29,21 +33,20 @@ for creator in root.findall('lifecycle/contribute'):
     if c == 1:
         PUBLISHER = d
     elif c == 2:
-        CREATOR = d
+        CREATOR = d.rstrip()
     c += 1
 #LICENSE = root.find('').text
 LANGUAGE = root.find('general/language').text
 #EDUCATIONLEVEL = root.find('').text
-ENTRY = root.find('general/identifier/entry').text
+if root.find('general/identifier/entry') != None:
+    ENTRY = root.find('general/identifier/entry').text
+else:
+	ENTRY = ''
 
-f = open(sys.argv[1] + '_rdf.txt', 'w')
+f = open(fileName + '_rdf.txt', 'w')
 f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/title",TITLE.encode('utf-8')))
 #f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/abstract",ABSTRACT.encode('utf-8')))
 f.write("<%s>\t<%s>\t\"%s\"^^xsd:date .\n" %(ENTRY,"http://purl.org/dc/terms/date",DATE.encode('utf-8')))
-f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/coursename",COURSENAME.encode('utf-8')))
-f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/coursecode",COURSECODE.encode('utf-8')))
-f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/keywords",KEYWORDS.encode('utf-8')))
-f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/taxonomy",TAXONOMY.encode('utf-8')))
 #f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/references",REFERENCES.encode('utf-8')))
 f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/description",DESCRIPTION.encode('utf-8')))
 f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/publisher",PUBLISHER.encode('utf-8')))
@@ -51,4 +54,8 @@ f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/creator",CREA
 #f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/license",LICENSE.encode('utf-8')))
 f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/language",LANGUAGE.encode('utf-8')))
 #f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/educationlevel",EDUCATIONLEVEL.encode('utf-8')))
+f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/coursename",COURSENAME.encode('utf-8')))
+if COURSECODE != None:
+	f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/coursecode",COURSECODE.encode('utf-8')))
+f.write("<%s>\t<%s>\t\"%s\" .\n" %(ENTRY,"http://purl.org/dc/terms/keywords",KEYWORDS.encode('utf-8')))
 f.close()
