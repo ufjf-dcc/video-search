325b1d090fef33b91780917dc0316700f2fbcad0
Sandro <sandroacoelho@gmail.com>
2014-08-19 07:51:36 -0300

    Update Languages.java

--- crawler/core/src/main/java/br/ufjf/languages/Languages.java
+++ crawler/core/src/main/java/br/ufjf/languages/Languages.java
@@ -1,7 +1,7 @@
 /*
 +--------------------------------------------------------------------------------------+
 |Federal University Of Juiz de Fora - UFJF - Minas Gerais - Brazil                     |
-|Institute of Exact Sciences - ICE                                                     |
+|Institute of Hard Sciences- ICE                                                     |
 |Computer Science Departament - DCC                                                    |
 |Project.........: qodra - video search                                                |
 |Created in......:12/12/2013                                                           |
